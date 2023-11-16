de42e034591d2f4d6386d0d02173c89b0ee43f88
dinesh <did37745@mail.com>
2023-11-17 00:58:56 +0530

    count value added

--- variable.tf
+++ variable.tf
@@ -14,13 +14,12 @@ variable "ami"{
     type = string
     default = "ami-05c13eab67c5d8861"
 }
-/*
+
 variable "instance_count"{
     description = "number of instant gonna create"
     type = number 
-    default = 10
+    default = 0
 }
-*/
 variable "name" {
     description = "Name for the AWS instance"
     type        = string
