9caaaab30b48eeb898073bbeaa10cb99bc63891c
Dinesh-SM <99877195+Dinesh-SM@users.noreply.github.com>
2024-03-23 22:17:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ resource "aws_instance" "app_server" {
   count             = var.instance_count
   ami               = var.ami
   instance_type     = var.instance_type
-  availability_zone = var.availability_zone
-
   tags = {
     Name = var.name
   }
